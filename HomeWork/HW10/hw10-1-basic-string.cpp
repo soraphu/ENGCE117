268b1c48989ca1ab93daa55de53fbb16c22939d7
Soraphu <143694376+soraphu@users.noreply.github.com>
2024-12-06 11:56:02 +0700

    Add files via upload

---
+++ HomeWork/HW10/hw10-1-basic-string.cpp
@@ -0,0 +1,21 @@
+/*
+    จงเขียนโปรแกรมเพื่อรับค่า String ที่สามารถรับชื่อและนามสกุลไว้ในตัวแปรเดียวกันได้ พร้อมกับแสดงผลบนหน้าจอคอมพิวเตอร์
+
+    Test case:
+        Kittinan Noimanee
+    Output:
+        Name: Kittinan Noimanee
+
+    Test case:
+        Visual Studio Code
+    Output:
+        Name: Visual Studio Code
+
+*/
+#include <stdio.h>
+
+int main() {
+    char Name[ 90 ] ;
+    gets( Name ) ;
+    printf( "Name: %s", Name ) ;
+}//end function
